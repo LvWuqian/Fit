9d8b9a05f2527972ee3b218fd667384fb436bb44
IA <331044216@qq.com>
2019-12-14 10:53:58 +0800

    把拍照上传嵌进项目里了。

--- src/main/java/org/csu/fit/controller/UploadController.java
+++ src/main/java/org/csu/fit/controller/UploadController.java
@@ -5,6 +5,7 @@ import org.csu.fit.config.OssConfiguration;
 import org.csu.fit.domain.Dishes;
 import org.csu.fit.service.DishService;
 import org.csu.fit.service.IFileService;
+import org.csu.fit.utils.StringToImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,26 +29,9 @@ public class UploadController {
             if(file != null){
                 String filename = file.getOriginalFilename();
                 if (!"".equals(filename.trim())){
-                    System.out.println("file size:" + file.getSize());
                     String uploadUrl = ossConfiguration.getPREFIX() + fileService.uploadPicture(file);
                     if(uploadUrl != null){
-
-                        JSONArray resList = dishService.getDishesByFilePath(uploadUrl);
-
-                        List<Dishes> ansList = new ArrayList<>();
-                        for(int i = 0; i < resList.size(); i ++){
-                            Dishes dishes = new Dishes();
-                            dishes.setTargetImage_url(uploadUrl);
-                            dishes.setName(resList.getJSONObject(i).getString("name"));
-                            dishes.setImage_url(resList.getJSONObject(i).getJSONObject("baike_info").getString("image_url"));
-                            dishes.setBaike_url(resList.getJSONObject(i).getJSONObject("baike_info").getString("baike_url"));
-                            dishes.setCalorie(resList.getJSONObject(i).getFloat("calorie"));
-                            dishes.setDescription(resList.getJSONObject(i).getJSONObject("baike_info").getString("description"));
-                            dishes.setProbability(resList.getJSONObject(i).getFloat("probability"));
-
-                            ansList.add(dishes);
-                        }
-                        return ansList;
+                        return getAnalysisResult(uploadUrl);
                     }
                 }
             }
@@ -56,4 +40,45 @@ public class UploadController {
         }
         return null;
     }
+
+
+    @PostMapping("/dish")
+    public List<Dishes> dishRecognise(@RequestParam("base64url") String base64String){
+        try {
+            MultipartFile file = StringToImageUtil.base64ToMultipart(base64String);
+            if(file != null){
+                String filename = file.getOriginalFilename();
+                if (!"".equals(filename.trim())){
+                    System.out.println("file size:" + file.getSize());
+                    String uploadUrl = ossConfiguration.getPREFIX() + fileService.uploadPicture(file);
+                    if(uploadUrl != null){
+                        return getAnalysisResult(uploadUrl);
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private List<Dishes> getAnalysisResult(String uploadUrl){
+        JSONArray resList = dishService.getDishesByFilePath(uploadUrl);
+
+        List<Dishes> ansList = new ArrayList<>();
+        for(int i = 0; i < resList.size(); i ++){
+            Dishes dishes = new Dishes();
+            dishes.setTargetImage_url(uploadUrl);
+            dishes.setName(resList.getJSONObject(i).getString("name"));
+            dishes.setImage_url(resList.getJSONObject(i).getJSONObject("baike_info").getString("image_url"));
+            dishes.setBaike_url(resList.getJSONObject(i).getJSONObject("baike_info").getString("baike_url"));
+            dishes.setCalorie(resList.getJSONObject(i).getFloat("calorie"));
+            dishes.setDescription(resList.getJSONObject(i).getJSONObject("baike_info").getString("description"));
+            dishes.setProbability(resList.getJSONObject(i).getFloat("probability"));
+
+            ansList.add(dishes);
+        }
+        return ansList;
+    }
+
 }
